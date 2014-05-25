a8c370f5ba53674ee8edf541ea259f5a74e79066
Jacob Gunnarsson <jacob.gunnarsson@hyperisland.se>
2014-05-25 13:43:14 -0400

    Added wesual base

---
+++ public/js/main.js
@@ -0,0 +1,31 @@
+var wesual = wesual || {};
+
+wesual = {
+	init: function() {
+		this.initFont()
+	},
+
+	initFont: function() {
+        var head = document.getElementsByTagName('head')[0];
+        	path = '//easy.myfonts.net/v2/js?sid=196848(font-family=Grumpy+Black+24)&key=zYSUJW1plZ',
+            protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+            script = document.createElement('script');
+
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = protocol + path;
+
+        head.appendChild(script);
+
+        var checkBanner = setInterval(function() {
+        	if ($('#mfPreviewBar').length) {
+        		$('#mfPreviewBar').remove();
+        		clearInterval(checkBanner);
+        	}
+        }, 10);
+	}
+}
+
+$(function() {
+	wesual.init();
+});
